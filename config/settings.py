02947706aa0a1eb40c322ba35b50470d6f29fc86
sheikh-python <ahmadsheikhzadeh902@gmail.com>
2024-02-25 09:17:03 +0330

    password change and reset templates

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,7 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 
