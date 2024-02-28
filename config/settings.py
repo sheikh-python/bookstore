820f301324902fc9abe25a91e53e3ac4d439c115
sheikh-python <ahmadsheikhzadeh902@gmail.com>
2024-02-28 08:22:48 +0330

    base dir in settings for static file

--- config/settings.py
+++ config/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -132,5 +132,6 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
