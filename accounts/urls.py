6149d085293294dfe801eacc881154f927cc0a1e
sheikh-python <ahmadsheikhzadeh902@gmail.com>
2024-03-01 10:10:21 +0330

    logout and link on header

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.SignUpView.as_view(), name='signup')
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
-
 from django.views import generic
 from .forms import CustomUserCreationForm
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.contrib.auth.views import LogoutView
 
 
 class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
+
+
